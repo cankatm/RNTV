53373d0435ba95ee251b0c2010893c1836b11ee7
mert cankat <mert@JotForms-MacBook-Pro.local>
2020-11-11 17:40:58 +0300

    DummyMovieData :: images added

--- src/helpers/dummyMovieData.js
+++ src/helpers/dummyMovieData.js
@@ -1,4 +1,7 @@
+import SecretWindowImage from '../../assets/images/secret_window.jpg';
+import ShutterIslandImage from '../../assets/images/shutter_island.jpg';
+
 export default DummyMovieData = [
-  {id: 1, name: 'Secret Window', imgSrc: null},
-  {id: 2, name: 'Shutter Island', imgSrc: null},
+  {id: 1, name: 'Secret Window', imgSrc: SecretWindowImage},
+  {id: 2, name: 'Shutter Island', imgSrc: ShutterIslandImage},
 ];
