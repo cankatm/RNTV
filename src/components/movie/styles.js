84aeb2148d1ebe7bdab4b1224d34143414ac54ef
mert cankat <mert@JotForms-MacBook-Pro.local>
2020-11-11 17:53:57 +0300

    MovieItem :: styles changed

--- src/components/movie/styles.js
+++ src/components/movie/styles.js
@@ -6,13 +6,14 @@ export default StyleSheet.create({
   movieItemFrame: {
     padding: 8,
     marginHorizontal: 16,
+    alignItems: 'center',
   },
   movieItemImage: {
     width: 200,
     height: 300,
   },
   movieItemText: {
-    fontSize: 24,
+    fontSize: 16,
     color: colors.creamWhite,
     marginTop: 8,
   },
