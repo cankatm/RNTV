549e350f91cb328820508f46e47ec4c51dab89ce
mert cankat <mert@JotForms-MacBook-Pro.local>
2020-11-11 17:01:54 +0300

    MovieItem :: styles added

--- src/components/movie/MovieItem.js
+++ src/components/movie/MovieItem.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 
+import styles from './styles';
+
 const MovieItem = () => {
   return (
-    <View>
-      <Text></Text>
+    <View style={styles.movieItemContainer}>
+      <Text>Movie Item</Text>
     </View>
   );
 };
---
+++ src/components/movie/styles.js
@@ -0,0 +1,9 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  movieItemContainer: {
+    width: 200,
+    height: 300,
+    backgroundColor: 'red',
+  },
+});
