ec7fb76334e3ccb619988006f2ceba68cc7be39e
mert cankat <mert@JotForms-MacBook-Pro.local>
2020-11-11 18:56:07 +0300

    App :: removed unnecessary Text

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View} from 'react-native';
 
 import {ControllerProvider} from './src/context/ControllerContext';
 import HomeScreen from './src/screens/HomeScreen';
