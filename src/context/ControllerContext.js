5cc2e775df60e5e7b5c307ef39cece41d5ef7d23
mert cankat <mert@JotForms-MacBook-Pro.local>
2020-11-11 18:11:13 +0300

    ControllerContet :: useLocalStore added

--- src/context/ControllerContext.js
+++ src/context/ControllerContext.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import {useLocalStore} from 'mobx-react';
 
 import {createControllerStore} from '../store/controllerStore';
 
 const ControllerContext = React.createContext(null);
 
 export const ControllerProvider = ({children}) => {
+  const controllerStore = useLocalStore(createControllerStore());
+
   return (
-    <ControllerContext.Provider value={createControllerStore}>
+    <ControllerContext.Provider value={controllerStore}>
       {children}
     </ControllerContext.Provider>
   );
