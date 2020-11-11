a346ff0e6b7b277eafda1f2310d78bf7a547fa22
mert cankat <mert@JotForms-MacBook-Pro.local>
2020-11-11 17:04:23 +0300

    Screens :: styles added

---
+++ src/screens/styles.js
@@ -0,0 +1,10 @@
+import {StyleSheet} from 'react-native';
+
+import * as colors from '../helpers/colors';
+
+export default StyleSheet.create({
+  homeScreenContainer: {
+    flex: 1,
+    backgroundColor: colors.nightBlue,
+  },
+});
