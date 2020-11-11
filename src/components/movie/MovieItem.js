1804b39d871a86d577ca4ec05531816700d99dc7
mert cankat <mert@JotForms-MacBook-Pro.local>
2020-11-11 17:14:17 +0300

    MovieItem :: make it pressable

--- src/components/movie/MovieItem.js
+++ src/components/movie/MovieItem.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, TouchableOpacity} from 'react-native';
 
 import styles from './styles';
 
 const MovieItem = () => {
   return (
-    <View style={styles.movieItemContainer}>
-      <Text>Movie Item</Text>
-    </View>
+    <TouchableOpacity>
+      <View style={styles.movieItemContainer}>
+        <Text>Movie Item</Text>
+      </View>
+    </TouchableOpacity>
   );
 };
 
