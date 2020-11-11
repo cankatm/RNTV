706d9256b98a56daf40227730174c9e9463a6474
mert cankat <mert@JotForms-MacBook-Pro.local>
2020-11-11 17:51:28 +0300

    MovieItem :: removed unnecessary background colors

--- src/components/movie/styles.js
+++ src/components/movie/styles.js
@@ -5,13 +5,11 @@ import * as colors from '../../helpers/colors';
 export default StyleSheet.create({
   movieItemFrame: {
     padding: 8,
-    backgroundColor: 'blue',
     marginHorizontal: 16,
   },
   movieItemImage: {
     width: 200,
     height: 300,
-    backgroundColor: 'red',
   },
   movieItemText: {
     fontSize: 24,
