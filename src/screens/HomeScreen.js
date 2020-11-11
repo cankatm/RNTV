96f3182c8c2ce483ab40ece7042823299db14990
mert cankat <mert@JotForms-MacBook-Pro.local>
2020-11-11 17:02:37 +0300

    HomeScreen :: MovieItem added

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 
+import {MovieItem} from '../components/movie';
 import * as colors from '../helpers/colors';
 
 const HomeScreen = () => {
   return (
     <View style={{flex: 1, backgroundColor: colors.nightBlue}}>
-      <Text>Home screen</Text>
+      <MovieItem />
     </View>
   );
 };
