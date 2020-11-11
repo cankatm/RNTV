c49b1a67d522c0651befae48c51227326a83593d
mert cankat <mert@JotForms-MacBook-Pro.local>
2020-11-11 16:55:45 +0300

    App :: homeScreen added

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 
+import HomeScreen from './src/screens/HomeScreen';
+
 const App = () => {
   return (
-    <View>
-      <Text></Text>
+    <View style={{flex: 1}}>
+      <HomeScreen />
     </View>
   );
 };
