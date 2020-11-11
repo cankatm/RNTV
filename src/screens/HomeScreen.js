e7b1d7f411fc89685519f869a110222357961968
mert cankat <mert@JotForms-MacBook-Pro.local>
2020-11-11 16:57:10 +0300

    HomeScreen :: fullScreen and blue color added

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 
+import * as colors from '../helpers/colors';
+
 const HomeScreen = () => {
   return (
-    <View>
-      <Text></Text>
+    <View style={{flex: 1, backgroundColor: colors.nightBlue}}>
+      <Text>Home screen</Text>
     </View>
   );
 };
